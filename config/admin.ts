c22e48ca8f5b1594d8f1210b94788ee3ccc98d97
Muskan <muskan.singh@isitca.com>
2024-08-14 02:49:40 +0530

    first commit

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', "19kNfnnv+Cq/XwjlKLuOsg=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', "2nioXdV5pPo72DQT30ETlA=="),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', "uuv/RRFg4c/2m1UmgiUxFw=="),
     },
   },
   flags: {
--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || ["Yl8646SuG3M0c5cESeJiFg==","fYS3yR1gPATHYXqREG2U9Q==","yLwMUwJtTYf4bjoSwdc6Vg==","hdK1RrIz7V1jgW6jr/Ab4Q=="],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
